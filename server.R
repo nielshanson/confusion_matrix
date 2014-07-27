0efb1732d92d5605d161ad7ae4f1458eea08cc85
Niels Hanson <nielsh@mail.ubc.ca>
2014-07-26 17:35:59 -0700

    Added confusion table and ROC curve.

--- server.R
+++ server.R
@@ -1,9 +1,16 @@
 library(shiny)
 library(ggplot2)
+library(flux)
 
 diabetesRisk <-  function(glucose) glucose / 200
 
-draw_plot <- function(mu_0, sigma_0, mu_a, sigma_a, lin, rev_order) {
+my.env <- new.env()
+my.env$ROCvalues_x <- NULL # x,y,lin values of the ROC curve
+my.env$ROCvalues_y <- NULL
+my.env$ROClin <- NULL
+my.env$ROC_param <- NULL
+
+draw_distribution_plot <- function(mu_0, sigma_0, mu_a, sigma_a, lin, rev_order) {
     temp_range <- c(mu_0-(4*sigma_0), mu_0+(4*sigma_0), 
                     mu_a-(4*sigma_a), mu_a+(4*sigma_a))
     xvals <- seq(min(temp_range), max(temp_range), length = 1000)
@@ -72,26 +79,94 @@ draw_plot <- function(mu_0, sigma_0, mu_a, sigma_a, lin, rev_order) {
             annotate("text", label=paste("H[0]"), parse=TRUE, x = mu_a, y = max(y1) + 0.025, size = 8, colour = "black")
     }
     g_plot
+    
 }
 
+create_confusion_table <- function(mu_0, sigma_0, mu_a, sigma_a, lin) {
+    # calculate areas of TP, TN, FP, FN
+    
+    TP_prob = round(pnorm(lin, mean=mu_a, sd=sigma_a, lower.tail=FALSE), 4)
+    FN_prob = round(pnorm(lin, mean=mu_a, sd=sigma_a, lower.tail=TRUE), 4)
+    TN_prob = round(pnorm(lin, mean=mu_0, sd=sigma_0, lower.tail=TRUE), 4)
+    FP_prob = round(pnorm(lin, mean=mu_0, sd=sigma_0, lower.tail=FALSE), 4)
+    
+    sens <- TP_prob / (TP_prob + FN_prob)
+    spec <- TN_prob / (TN_prob + FP_prob)
+    ppv <- TP_prob / (TP_prob + FP_prob)
+    npv <- TN_prob / (TN_prob + FN_prob)
+    
+    acc <- (TP_prob + TN_prob) / (TP_prob + FN_prob + TN_prob + FP_prob)
+    
+    confusion_table = data.frame(mu=c(TP_prob,FN_prob, sens), sd=c(FP_prob, TN_prob, spec), c(ppv, npv, acc))
+    rows <- c("Test_T", "Test_F", "Sensitivity / Specificity")
+    cols <- c("Observed_T", "Observed_F", "Precission (PPV) / Negative Predictive Value (NPV) / Accuracy")
+    row.names(confusion_table) <- rows
+    colnames(confusion_table) <- cols
+    
+    confusion_table
+}
+
+create_ROC_curve <- function(mu_0, sigma_0, mu_a, sigma_a, lin) {
+    # calcualte the x,y value of the current value of lin
+    
+    
+    # check to see if the distributions changed
+    parameters <- c(mu_0, sigma_0, mu_a, sigma_a)
+    if (is.null(my.env$ROC_param)) {
+        my.env$ROC_param <- parameters
+    }
+    
+    if (sum(my.env$ROC_param == parameters) != length(parameters)) {
+        # some parameter changed, reset the ROC values
+        my.env$ROCvalues_x <- NULL # x,y values of the ROC curve
+        my.env$ROCvalues_y <- NULL
+        my.env$ROClin <- NULL
+        my.env$ROC_param <- NULL
+    }
+    
+    if( !(lin %in% my.env$ROClin) ){
+        # calculate ROC values
+        
+        TP_prob = pnorm(lin, mean=mu_a, sd=sigma_a, lower.tail=FALSE)
+        FN_prob = pnorm(lin, mean=mu_a, sd=sigma_a, lower.tail=TRUE)
+        TN_prob = pnorm(lin, mean=mu_0, sd=sigma_0, lower.tail=TRUE)
+        FP_prob = pnorm(lin, mean=mu_0, sd=sigma_0, lower.tail=FALSE)
+        
+        sens <- TP_prob / (TP_prob + FN_prob)
+        spec <- TN_prob / (TN_prob + FP_prob)
+        
+        my.env$ROCvalues_x <- c(my.env$ROCvalues_x, round(1-spec,4))
+        my.env$ROCvalues_y <- c(my.env$ROCvalues_y, round(sens,4))
+        my.env$ROClin <- c(my.env$ROClin, lin)
+    }
+    
+    # plot current ROC curve
+    temp_df <-data.frame(fpr=my.env$ROCvalues_x, sens=my.env$ROCvalues_y, decision=my.env$ROClin)
+    if(length(temp_df$fpr) > 1) {
+        temp_auc <- round(auc(temp_df$fpr, temp_df$sens),4)
+    } else {
+        temp_auc <- 0
+    }
+    
+    ggplot(temp_df, aes(x=fpr, y=sens)) + 
+           theme_bw(base_family = "Gill Sans", base_size = 14) +
+           geom_line(size=1.3) + 
+           geom_point(aes(x=fpr, y=sens, size=2), color="blue") +
+           geom_text(aes(label=decision), hjust=1.5, vjust=-1, family="Gill Sans") +
+           xlim(0,1.0) +
+           ylim(0,1.0) +
+           xlab("False Positive Rate (1-Specificity)") +
+           ylab("Sensitivity") + 
+           annotate("text", label=paste("AUC=", temp_auc, sep=" "), x=0.8, y=0.2, family = "Gill Sans") +
+           theme(legend.position="none")
+
+}
 
 shinyServer(
     function(input, output) {
-        output$oid1 <- renderPrint({input$id1})
-        output$oid2 <- renderPrint({input$id2})
-        output$odate <- renderPrint({input$date})
-        
-        # diabetes stuff
-        output$inputValue <- renderPrint({input$glucose})
-        output$prediction <- renderPrint({diabetesRisk(input$glucose)})
-        output$newHist <-  renderPlot({
-            hist(sample(1:30, 100, replace=TRUE))
-            }
-        )
-        output$odate <- renderPrint({input$rev_order})
-        # mean model stuff
+        # create distribution plot
         output$newPlot <- renderPlot({
-            draw_plot(input$mu_0,
+            draw_distribution_plot(input$mu_0,
                       input$sd_0,
                       input$mu_a,
                       input$sd_a,
@@ -99,7 +174,20 @@ shinyServer(
                       input$rev_order)
         })
         
-        output$table <- renderTable({data.frame(mu=c(input$mu_0,input$mu_a), sd=c(input$sd_0, input$sd_a))})
+        # create table
+        output$table <- renderTable({ create_confusion_table(input$mu_0,
+                                                             input$sd_0,
+                                                             input$mu_a,
+                                                             input$sd_a,
+                                                             input$lin) })
         
-    }
-)
+        
+        # create ROC curive with values
+        output$ROC_plot <- renderPlot({
+            create_ROC_curve(input$mu_0,
+                             input$sd_0,
+                             input$mu_a,
+                             input$sd_a,
+                             input$lin)
+        })
+})
--- ui.R
+++ ui.R
@@ -1,24 +1,18 @@
 library(shiny)
 shinyUI(pageWithSidebar(
-    headerPanel("Data Science for the Win!"),
+    headerPanel("De-confusing the Confusion Table"),
     sidebarPanel(
         # markup
-        h1('Sidebar panel'),
-        h1('H1 Text'),
-        h2('H2 Text'),
-        h3('H3 Text'),
-        h4('H4 Text'),
-        numericInput('id1', 'Numeric input, labeled id1', 0, min = 0, max = 10, step =1),
-        checkboxGroupInput("id2", "Checkbox",
-                           c("Value 1" = "1",
-                             "Value 2" = "2",
-                             "Value 3" = "3")),
-        dateInput("date", "Date:"),
+        h2('Parameters'),
+        h5('Null Distribution'),
         numericInput('mu_0', 'Null Mean', 0, min=-100, max=100, step=0.2),
         numericInput('sd_0', 'Null Standard Deviation', 1, min=0.1, max=100, step=0.1),
+        h5('Alternative Distribution'),
         numericInput('mu_a', 'Alternative Mean', 2, min=-100, max=100, step=0.2),
         numericInput('sd_a', 'Alternative Standard Deviation', 1, min=0.1, max=100, step=0.2),
+        h5('Classifier'),
         numericInput('lin', 'Decision Boundary', 1, min=-100, max=100, step=0.2),
+        h5('Other Options'),
         checkboxGroupInput("rev_order", "Reverse Null and Alternative", c("Yes" = "yes")),
         submitButton('Submit')
         ),
@@ -28,6 +22,9 @@ shinyUI(pageWithSidebar(
           also show in the graph the relative areas of correct outcomes, True Positives (TP) and 
           True Negatives (TN),  and incorrect outcomes, Type-1 Errors or False Positives (FP) and 
           Type-2 Errors or False Negatives (FN).'),
+          p(em("This figure plots the Null and Alternative distributions according to their current
+                paramters and decision boundary. Try changing the mean, standard deveations,
+                and the decision boundary to update the figures and tables below.")),
 #         h4('You entered'),
 #         verbatimTextOutput('oid1'),
 #         h4('You entered'),
@@ -41,16 +38,35 @@ shinyUI(pageWithSidebar(
 #         h4('Which resulted in a prediction of '),
 #         verbatimTextOutput('prediction'),
           plotOutput('newPlot'),
-          # TODO Put significance and power
           h4('Performance Evaluation with Confusion Tables'),
           p(withMathJax("A common task in statistics and in particular the increasingly popular area of
                        Machine Learning is the assessment of a binary classifier like the simplisitic
-                       one above. However, performance is often a balance between the two error types
-                       and can be somewhat multi-faceted depending on the potential application concerns
-                       of the classifier. Thus, it is convenient to summarize the performance of a classifer
-                       with a number of different statistics in what is commonly called a Confusion Table 
-                       shown below: $$a + b$$")),
+              one above. However, performance is often a balance between the two error types
+              and can be somewhat multi-faceted depending on the potential application concerns
+              of the classifier.")),
+          p(em("This table shows the areas and derived performance statistics of the current 
+               distributions and decision boundary above.")),
+          p(strong(("Warning: Doesn't work properly when Null and Alternative are reversed."))),
           tableOutput("table"),
-          withMathJax("Some math here $$\\alpha+\\beta$$")
+          h4('Performance Evaluation with Reciever Operating Characteristic Curves (ROC-Curve)'),
+          p('One common way of evaluating the overall effectiveness of a binary classifer 
+             accross decision boundaries is via an ROC curve. The True Positive Rate Specificity 
+             is plotted against the False Positive Rate (1-Sensitivity). Classifiers with curves
+             that plot closer to the upper left-hand corner are considered better (a random classifer
+             is a diagonal line). One statistic to assess a classifier\'s performance here is the Area
+             Under the Curve. Here this esimate is generated using the Trapezoidal Rule.'),
+          p(em("The plot below generates the ROC curve for the current classifier one point at a time. 
+               Alter the decision boundary while keeping all other settings constant and the ROC curve
+               will be generated between the ROC points currently calculated. Changing the distribution
+               parameters will reset the plot.")),
+          p(strong(("Warning: Doesn't work properly when Null and Alternative are reversed."))),
+          plotOutput('ROC_plot')#,
+#           h4('Discussion of Confusion Table Statistics'),
+#           p(withMathJax("A variety of statistics exist to summarize the performance of a binary classifer based on its goals. 
+#                          In particular is usually a tradoff between the cost of making a particular type of error (False 
+#                          Positives or False Negatives). Its best to discuss each of these statistics separately: 
+#                          $$\\mbox{True Positives (TP)} \\approx P(T|T) = P(H_a|H_a)$$
+#                         
+#                         "))
     )
 ))
